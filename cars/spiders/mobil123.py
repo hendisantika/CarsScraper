a45c971335d256364a3916b16b2208f7aa00f814
hendisantika <hendisantika@gmail.com>
2016-11-07 19:22:45 +0700

    Fix mobil123 bekas crawler

--- cars/spiders/mobil123.py
+++ cars/spiders/mobil123.py
@@ -1,85 +1,144 @@
-#UDAH OKE UNTUK INSERT INTO MYSQL DB NYA MAH
-#TINGGAL NEXT PAGE
+#Scrapy Python
+#Finished @ Friday, November 4th 2016 13.30 WIB
+#Created by : Hendi Santika
+#Waslap / Telegram : +6281321411881
+#Skype : hendi.santika 
 
 import scrapy
 import MySQLdb
 import re
 from tld import get_tld
+from string import Template
+import time
 
 class Tes1(scrapy.Spider):
     name = "mobil123"
     start_urls = [
-        'http://www.mobil123.com/mobil?type=used',
+        'http://www.mobil123.com/mobil?type=used'
     ]
 
     def __init__(self):
         self.db = MySQLdb.connect("127.0.0.1", "root", "root", "olx")
-        self.stmt = "insert into cars(url, title, price, posted, city, province, contact_person, description, source_site, year, merek, type, old_new) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        self.stmt = "insert into cars(url, title, price, posted, city, province, contact_person, description, source_site, year, transmission, brand, model, type, ownership, nego, uploaded_by, phone, seen) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
     def parse(self, response):
-        urls = response.xpath('//*[@id="listing_3232491"]/div/div[2]/h2/a').extract()
-
+        # urls = response.xpath('//*[@id="listing_3232491"]/div/div[2]/h2/a/@href').extract()
+        urls = response.xpath('//article[contains(@class, "listing")]/div/div[2]/h2/a/@href').extract()
         for url in urls:
             absolute_url = response.urljoin(url)
             request = scrapy.Request(
                 absolute_url, callback=self.parse_cars)
             yield request
+            time.sleep(0.4)
 
         # process next page
-        # next_page_url = response.xpath('/html/body/div[2]/div/div[4]/div[3]/div[1]/div/a[@class="next_page"]/@href').extract_first() #UDAH OK NICH NEXT PAGE NYA
-        #next_page_url = response.xpath('/html/body/div[2]/div/div[4]/div[3]/div[1]/div/a[8]/@href').extract_first() #YANG INI BUAT NGETES DOANG
-        next_page_url = response.xpath('//a[@class="next_page"]/@href').extract_first()
+        # next_page_url = response.xpath('//*[@id="body-container"]/div/div/div/span/a/@href').extract_first()
+        # next_page_url = response.xpath('//*[@id="body-container"]/div/div/div[2]/span[16]/a/@href').extract_first() # YANG INI UNTUK NGETES DOANG
+        next_page_url = response.xpath('//*[@id="classified-listings-result"]/div[6]/ul/li[6]/a/@href').extract_first() #YANG INI UDAH OK NEXT PAGE NYA NICH
         absolute_next_page_url = response.urljoin(next_page_url)
         request = scrapy.Request(absolute_next_page_url)
         yield request
 
     def parse_cars(self, response):
         c = self.db.cursor()
-        title = response.xpath(
-            '//*[@id="listing_3232491"]/div[2]/div[1]/div[1]/div/div[1]/h1/a/text()').extract_first()
-        price = response.xpath(
-            '//*[@id="listing_3232491"]/div[2]/div[1]/div[1]/div/div[2]/p[2]/text()').extract_first().strip()
-        # price = re.sub('[Rp. ]', "", price_tmp)
-        city = '\n'.join(response.xpath(
-            #'/html/body/div[3]/div/table/tbody/tr/td[1]/table[1]/tbody/tr/td[3]/text()').extract_first()
-            '//*[@id="listing_3232491"]/div[2]/div[2]/div[1]/div/div/div[2]/text()').extract()).strip()
-        posted = '\n'.join(response.xpath(
-            #'/html/body/div[3]/div/table/tbody/tr/td[1]/table[1]/tbody/tr/td[2]/text()').extract_first()
-            '//*[@id="listing_3232491"]/div[2]/div[1]/div[1]/div/div[1]/div[2]/text()').extract()).strip()
-        cp = response.xpath(
-            '/html/body/div[3]/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/div/div[2]/div[1]/div[1]/a/text()').extract_first()
-        desc = '\n'.join(response.xpath(
-            #'/html/body/div[3]/div/table/tbody/tr/td[1]/div[4]/text()').extract_first()
-            '//div[@class="ad_show_detail"]/text()').extract()).strip().replace('<br>', '\n')
-        ss = get_tld(response.url)
-        year = response.xpath(
-                '//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[4]/span[2]/text()').extract_first()
-        tipe = response.xpath(
-                    #'/html/body/div[3]/div/table/tbody/tr/td[1]/div[4]/div[2]/table/tbody/tr/td[1]/div/text()').extract_first()
-                '//td[@class="variant_td"]/text()').extract_first()
-        tipe = tipe.strip() if tipe is not None else ''
-        old_new = '\n'.join(response.xpath(
-                    #'/html/body/div[3]/div/table/tbody/tr/td[1]/table[1]/tbody/tr/td[1]/text()').extract_first()
-                    '//td[@class="second-hand"]/text()').extract()).strip()
-        merek = ''
+        title = ' '.join(response.xpath('//article[contains(@class,"listing")]/div[2]/div[1]/div[1]/div/div[1]/h1/a/text()').extract()).strip()
+            
+        price_tmp = response.xpath('//article[contains(@class,"listing")]/div[2]/div[1]/div[1]/div/div[2]/p[2]/text()').extract_first().strip()
+        price = re.sub('[Rp. ]', "", price_tmp)
+        city_tmp1 = response.xpath('//article[contains(@class,"listing")]/div[2]/div[2]/div[1]/div/div/div[2]/text()').extract_first().strip()
+        # city = response.xpath('//article[contains(@class,"listing")]/div[2]/div[2]/div[1]/div/div/div[2]/text()').extract_first()
+        # province = response.xpath('//article[contains(@class,"listing")]/div[2]/div[2]/div[1]/div/div/div[2]/text()').extract_first()
+        # city_tmp2 = city_tmp1.split("\>>")
+        # city_tmp2 = city_tmp1.split("\xbb")
+        # city_tmp2 = re.findall(r"[\w']+", city_tmp1)
+        # city = city_tmp2[1].strip()
+        # province = city_tmp2[0].strip()
+        # posted_tmp = map(unicode.strip, response.xpath('//*[@id="offer_active"]/div[3]/div[1]/div[1]/div[1]/p/small/span/text()').extract_first())
+        # posted_tmp = response.xpath('//*[@id="offer_active"]/div/div/div/div/p/small/span/text()').extract()
+        # posted_tmp = response.xpath('//*[@id="offer_active"]/div/div/div/div/p/small/span/text()').extract()
+        # posted_tmp = response.xpath('//*[@id="offer_active"]/div/div/div/div/p/small/span/text()').extract()
+        posted_tmp = (' '.join(map(unicode.strip, response.xpath('//*[@id="listing_3232491"]/div[2]/div[1]/div[1]/div/div[1]/div[2]/text()').extract())))
+        # posted_tmp = response.xpath('normalize-space(//*[@id="offer_active"]/div/div/div/div/p/small/span/text()').extract()
+            # '//*[@id="offer_active"]/div[3]/div[1]/div[1]/div[1]/p/small/span/text()').extract_first().strip()
+        # posted = posted_tmp
+        # posted = re.sub('sejak,', "", posted_tmp)
+        posted = posted_tmp 
+        # b = {'sejak ': '', ',' : '', '  ' : ' '}
+        # Pattern untuk tanggal posting
+        # b = {'Ditambahkan': '', 'sejak': '', ',' : '', '  ' : ' '}
+        # for x,y in b.items():
+        #     posted = posted.replace(x, y).strip()
 
-        # c.execute("insert into cars(url, title, price, posted, city, contact_person, description, source_site, year, merek, type, old_new) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
-        #          (response.url, title, price, posted, city, cp, desc, ss, year, merek, tipe, old_new))
+        # cp = ''
+        # cp = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[1]/div/div/div[1]/text()').extract_first().strip()
+        cp = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[1]/div/div/div[1]/text()').extract_first()
+        desc = ''
+        # desc = response.xpath(
+        #     '//*[@id="textContent"]/p/text()').extract_first().strip()
+        ss = get_tld(response.url)
+        # year = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[4]/span[2]/text()').extract_first().strip()
+        year = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[4]/span[2]/text()').extract_first()
+        # transmission = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[6]/span[2]/text()').extract_first().strip()
+        transmission = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[6]/span[2]/text()').extract_first()
+        # brand = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[1]/span[2]').extract_first().strip()
+        brand = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[1]/span[2]').extract_first()
+        # model = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[2]/span[2]/text()').extract_first().strip()
+        model = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[2]/span[2]/text()').extract_first()
+        # tipe = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[3]/span[2]').extract_first().strip()
+        tipe = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[3]/span[2]').extract_first()
+        ownership = 'Used'
+        nego = ''
+        # eCap = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[5]/span[2]/text()').extract_first().strip()
+        eCap = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[5]/span[2]/text()').extract_first()
+        eType = ''
+        # color = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[10]/span[2]/text()').extract_first().strip()
+        color = response.xpath('//*[@id="listing_3232491"]/div[2]/div[2]/div[5]/div/p[10]/span[2]/text()').extract_first()
+        uploaded_by = ''
+        # uploaded_by = response.xpath(
+        #                 '//*[@id="offer_active"]/div[3]/div[1]/div[1]/div[1]/p/small/span/a/span/text()').extract_first().strip()
+        phone = ''
+        seen = ''
+        # seen = response.xpath(
+        #                 '//*[@id="offerbottombar"]/div[3]/strong/text()').extract_first().strip()
+
+
+        # print("url :", response.url)
+        # print("title :", title)
+        # print("Posted2 ", posted2)
+        # print("Posted3 ", posted3)
+        # print("posted :", posted)
+
+        # c.execute("insert into cars(url, title, price, posted, city, province, contact_person, description, source_site, year, transmission, brand, model, type, ownership, nego, uploaded_by, phone, seen) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+        #          (response.url, title, price, posted, city, province, cp, desc, ss, year, transmission, brand, model, tipe, ownership, nego, uploaded_by, phone, seen))
         # self.db.commit()    
 
         cars = {
-            'title': title,
-            'price': price,
-            'city': city,
-            'posted': posted,
-            'cp'    : cp,
-            'desc'  : desc,
-            'url': response.url,
-            'source_site ' : ss,    
-            'merek ': merek,   
-            'type ' :  tipe,   
-            'year ' :  year,   
-            'old ' :  old_new   
+            'url'           : response.url,
+            'title'         : title,
+            'price'         : price,
+            'city'          : city,
+            'province'      : province,
+            'posted'        : posted,
+            'cp'            : cp,
+            'desc'          : desc,
+            'url'           : response.url,
+            'source_site'   : ss,    
+            'transmission'  : transmission,
+            'brand'         : brand,
+            'model'         : model,    
+            'type'          : tipe,   
+            'year'          : year,   
+            'ownership'     : ownership,
+            'nego'          : nego,    
+            'uploaded_by'   : uploaded_by,   
+            'phone'         : phone,   
+            'seen'          : seen   
             }
-            # 'url': url}
         yield cars                 
+
+
+
+ 
+
+              
+
