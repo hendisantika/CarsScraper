22e48d6831e4fd519e7d47db78fee4080f77ca90
hendisantika <hendisantika@gmail.com>
2016-11-08 13:20:16 +0700

    Fix data items details in jualo.py

--- cars/spiders/jualo_cars.py
+++ cars/spiders/jualo_cars.py
@@ -27,46 +27,42 @@ class Tes1(scrapy.Spider):
 
         # process next page
         # next_page_url = response.xpath('/html/body/div[2]/div/div[4]/div[3]/div[1]/div/a[@class="next_page"]/@href').extract_first() #UDAH OK NICH NEXT PAGE NYA
-        #next_page_url = response.xpath('/html/body/div[2]/div/div[4]/div[3]/div[1]/div/a[8]/@href').extract_first() #YANG INI BUAT NGETES DOANG
-        next_page_url = response.xpath('//a[@class="next_page"]/@href').extract_first()
+        next_page_url = response.xpath('/html/body/div[2]/div/div[4]/div[3]/div[1]/div/a[8]/@href').extract_first() #YANG INI BUAT NGETES DOANG
+        # next_page_url = response.xpath('//a[@class="next_page"]/@href').extract_first()
         absolute_next_page_url = response.urljoin(next_page_url)
         request = scrapy.Request(absolute_next_page_url)
         yield request
 
     def parse_cars(self, response):
         c = self.db.cursor()
-        title = response.xpath(
-            '/html/body/div/div/table/tbody/tr/td/div[@class="ad_show_title"]/text()').extract_first()
-        price_tmp = response.xpath(
-            '//div[@class="real_price"]/text()').extract_first().strip()
+        title = response.xpath('//html/body/div/div/table/tbody/tr/td/div[@class="ad_show_title"]/text()').extract_first()
+        price_tmp = response.xpath('//div[@class="real_price"]/text()').extract_first().strip()
         price = re.sub('[Rp. ]', "", price_tmp)
-        city = '\n'.join(response.xpath(
-            #'/html/body/div[3]/div/table/tbody/tr/td[1]/table[1]/tbody/tr/td[3]/text()').extract_first()
-            '//td[@class="top_location"]/text()').extract()).strip()
-        posted = '\n'.join(response.xpath(
-            #'/html/body/div[3]/div/table/tbody/tr/td[1]/table[1]/tbody/tr/td[2]/text()').extract_first()
-            '//td[@class="top_timer"]/text()').extract()).strip()
-        # cp = response.xpath(
-        #     '/html/body/div/div/table/tbody/tr/td/table/tbody/tr/td/div/div/div/div/a/text()').extract_first()
-        cp = response.xpath(
-            '//html/body/div/div/table/tbody/tr/td/table/tbody/tr/td/div/div/div/div/a/text()').extract_first()
+        city_tmp1 = '\n'.join(response.xpath('//td[@class="top_location"]/text()').extract()).strip()
+        city_tmp2 = filter(None, re.split("[,]+", city_tmp1))
+        # city_tmp2 = re.findall(r"[\w']+", city_tmp1)
+        city = city_tmp2[0].strip()
+        province = city_tmp2[1].strip()
+            
+        posted = '\n'.join(response.xpath('//td[@class="top_timer"]/text()').extract()).strip()
+        cp = response.xpath('//html/body/div/div/table/tbody/tr/td/table/tbody/tr/td/div/div/div/div/a/text()').extract_first()
         cp = cp.strip() if cp is not None else ''
-        desc = '\n'.join(response.xpath(
+        desc = '\n'.join(response.xpath('//div[@class="ad_show_detail"]/text()').extract()).strip().replace('<br>', '\n')
             #'/html/body/div[3]/div/table/tbody/tr/td[1]/div[4]/text()').extract_first()
-            '//div[@class="ad_show_detail"]/text()').extract()).strip().replace('<br>', '\n')
+            
         ss = get_tld(response.url)
         year = ''
-        model = response.xpath(
+        model = response.xpath('/html/body/div[3]/div/table/tbody/tr/td[1]/div[4]/div[2]/table/tbody/tr/td[1]/div/text()').extract_first()
                     #'/html/body/div[3]/div/table/tbody/tr/td[1]/div[4]/div[2]/table/tbody/tr/td[1]/div/text()').extract_first()
-                '/html/body/div[3]/div/table/tbody/tr/td[1]/div[4]/div[2]/table/tbody/tr/td[1]/div/text()').extract_first()
+                
         model = model.strip() if model is not None else '' 
-        tipe = response.xpath(
+        tipe = response.xpath('//td[@class="variant_td"]/text()').extract_first()
                     #'/html/body/div[3]/div/table/tbody/tr/td[1]/div[4]/div[2]/table/tbody/tr/td[1]/div/text()').extract_first()
-                '//td[@class="variant_td"]/text()').extract_first()
+                
         tipe = tipe.strip() if tipe is not None else ''
-        old_new = '\n'.join(response.xpath(
+        ownership = '\n'.join(response.xpath('//td[@class="second-hand"]/text()').extract()).strip()
                     #'/html/body/div[3]/div/table/tbody/tr/td[1]/table[1]/tbody/tr/td[1]/text()').extract_first()
-                    '//td[@class="second-hand"]/text()').extract()).strip()
+                    
         model = ''
 
         # c.execute("insert into cars(url, title, price, posted, city, contact_person, description, source_site, year, model, type, old_new) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
@@ -77,6 +73,7 @@ class Tes1(scrapy.Spider):
             'title'         : title,
             'price'         : price,
             'city'          : city,
+            'province'      : province,
             'posted'        : posted,
             'cp'            : cp,
             'desc'          : desc,
@@ -85,7 +82,7 @@ class Tes1(scrapy.Spider):
             'model'         : model,    
             'type'          : tipe,   
             'year'          : year,   
-            'old'           : old_new,   
+            'ownership'     : ownership,   
             'url'           : response.url
             }
         yield cars                 
