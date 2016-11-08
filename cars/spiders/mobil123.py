a01eb8c73c1ffe097151719479ee1c97aa409768
hendisantika <hendisantika@gmail.com>
2016-11-08 20:32:20 +0700

    Fix data detail items and next page url errors

--- cars/spiders/mobil123.py
+++ cars/spiders/mobil123.py
@@ -19,7 +19,7 @@ class Tes1(scrapy.Spider):
 
     def __init__(self):
         self.db = MySQLdb.connect("127.0.0.1", "root", "root", "olx")
-        self.stmt = "insert into cars_test(url, title, price, posted, city, province, source_site, year, transmission, brand, model, type, ownership, engine_capacity, doors) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        self.stmt = "insert into cars_test2(url, title, price, posted, city, province, source_site, year, transmission, brand, model, type, ownership, engine_capacity, doors) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
     def parse(self, response):
         # urls = response.xpath('//*[@id="listing_3232491"]/div/div[2]/h2/a/@href').extract()
@@ -29,27 +29,35 @@ class Tes1(scrapy.Spider):
             request = scrapy.Request(
                 absolute_url, callback=self.parse_cars)
             yield request
-            time.sleep(0.4)
+            time.sleep(2)
 
         # process next page
-        # next_page_url = response.xpath('//*[@id="body-container"]/div/div/div/span/a/@href').extract_first()
-        # next_page_url = response.xpath('//*[@id="body-container"]/div/div/div[2]/span[16]/a/@href').extract_first() #YANG INI UDAH OK NEXT PAGE NYA NICH
+        next2 = 'http://www.mobil123.com/'
+        next_page_url = response.xpath('//*[@id="classified-listings-result"]/div[6]/ul/li[6]/a/@href').extract_first() #YANG INI UDAH OK NEXT PAGE NYA NICH
+        # next_page_url = response.xpath('//*[@id="body-container"]/div/div/div[2]/span[16]/a/@href').extract_first() # YANG INI UNTUK NGETES DOANG 
         # next_page_url = response.xpath('//*[@id="classified-listings-result"]/div[6]/ul/li[6]/a/@href').extract_first() # YANG INI UNTUK NGETES DOANG 
         # next_page_url = response.xpath('//article[contains(@class,"listing")]/div[6]/ul/li[6]/a/@href').extract_first() # YANG INI UNTUK NGETES DOANG 
-        next_page_url = response.xpath('//*[@id="classified-listings-result"]/div[6]/ul/li[6]/a/@href').extract_first() # YANG INI UNTUK NGETES DOANG 
+        # next_page_url = response.xpath('//*[@id="classified-listings-result"]/div[6]/ul/li[6]/a/@href').extract_first() # YANG INI UNTUK NGETES DOANG 
+        
+
+
         absolute_next_page_url = response.urljoin(next_page_url)
+        # Supaya gak kena rate limits
+        time.sleep(2)
         request = scrapy.Request(absolute_next_page_url)
+        print"next url : ", absolute_next_page_url
         yield request
 
     def parse_cars(self, response):
         c = self.db.cursor()
         title = ' '.join(response.xpath('//article[contains(@class,"listing")]/div[2]/div[1]/div[1]/div/div[1]/h1/a/text()').extract()).strip()
         # price_tmp = response.xpath('//article[contains(@class,"listing")]/div[2]/div[1]/div[1]/div/div[2]/p[2]/text()').extract_first()
-        price_tmp = response.xpath('//*[@id="listing_3232491"]/div[2]/div[1]/div[1]/div/div[2]/p[@class="listing__price delta flush"]').extract_first()
+        # price_tmp = response.xpath('//*[@id="listing_3232491"]/div[2]/div[1]/div[1]/div/div[2]/p[@class="listing__price delta flush"]').extract_first()
+        price_tmp = response.xpath('//article[contains(@class,"listing--details")]/div[2]/div/div/div/div[2]/p[contains(@class,"listing__price")]/text()').extract_first().strip()
         # price_tmp = response.xpath('//*[@id="listing_3232491"]/div[2]/div[1]/div[1]/div/div[2]/p[2]/text()').extract_first()
         # price_tmp = price_tmp.strip() if price_tmp is not None else 'Hai Sayang' 
-        # price = re.sub('[Rp. ]', "", price_tmp)
-        price = price_tmp
+        price = re.sub('[Rp. ]', "", price_tmp)
+        # price = price_tmp
         city_tmp1 = response.xpath('//article[contains(@class,"listing")]/div[2]/div[2]/div[1]/div/div/div[2]/text()').extract_first().strip()
         city_tmp1 = city_tmp1.replace(u'\xbb', '|')
         # string.replace(u'\xbb', u'&raquo;')
@@ -130,9 +138,10 @@ class Tes1(scrapy.Spider):
         # print("Posted3 ", posted3)
         # print("doors : ",  doors)
 
-        # c.execute("insert into cars_test(url, title, price, posted, city, province, source_site, year, transmission, brand, model, type, ownership, engine_capacity, doors) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
-        #          (response.url, title, price, posted, city, province, ss, year, transmission, brand, model, tipe, ownership, eCap, doors))
-        # self.db.commit()    
+        c.execute("insert into cars_test2(url, title, price, posted, city, province, source_site, year, transmission, brand, model, type, ownership, engine_capacity, doors) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+                 (response.url, title, price, posted, city, province, ss, year, transmission, brand, model, tipe, ownership, eCap, doors))
+        # time.sleep(1)
+        self.db.commit()    
 
         cars = {
             'url'           : response.url,
