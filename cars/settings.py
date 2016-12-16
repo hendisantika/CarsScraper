9800324604726fd4a34f8e1acaf99eb218a09d49
hendisantika <hendisantika@gmail.com>
2016-12-16 09:00:07 +0700

    Setting Delta Fetch

--- cars/settings.py
+++ cars/settings.py
@@ -46,15 +46,12 @@ DOWNLOAD_DELAY = 0.5
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-SPIDER_MIDDLEWARES = {
-#    'cars.middlewares.MyCustomSpiderMiddleware': 543,
-#    'cars.DeltaFetch': 100,
-    'scrapy_deltafetch.DeltaFetch': 100,
-#    'cars.DeltaFetch': 100,
-}
+# SPIDER_MIDDLEWARES = {
+#     'scrapy_deltafetch.DeltaFetch': 100, # yang ini nich yang OK mah da
+# }
 
-DELTAFETCH_ENABLED = True
-DELTAFETCH_DIR = '/home/hendisantika/Documents/python/cars/cars/logs'
+# DELTAFETCH_ENABLED = True
+# DELTAFETCH_DIR = '/home/hendisantika/Documents/python/cars/cars/logs'
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
