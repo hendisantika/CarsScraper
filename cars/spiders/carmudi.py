1f652494cd204c9c47017f4a5b15fdec67a5429c
hendisantika <hendisantika@gmail.com>
2016-12-23 09:53:16 +0700

    Bugfix carmudi

--- cars/spiders/carmudi.py
+++ cars/spiders/carmudi.py
@@ -17,8 +17,10 @@ class Tes1(scrapy.Spider):
     ]
 
     def __init__(self):
-        self.db = MySQLdb.connect("127.0.0.1", "root", "root", "olx")
-        self.stmt = "insert into cars(url, title, price, posted, city, province, contact_person, description, source_site, year, transmission, brand, model, type, ownership, nego, uploaded_by, phone, seen) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        self.db = MySQLdb.connect("127.0.0.1", "root", "root", "scrapyDB")
+        self.stmt = "insert into cars(url, title, price, posted, city, province, contact_person, description, " \
+                    "source_site, year, transmission, brand, model, type, ownership, nego, uploaded_by, phone, seen) " \
+                    "values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
     def parse(self, response):
         urls = response.xpath('//section[contains(@class, "catalog-listing")]//h5[contains(@class, "item-title")]/a/@href').extract()
@@ -50,18 +52,35 @@ class Tes1(scrapy.Spider):
         province = ' '.join(response.xpath('//div[@id="addressBlock"]/address/text()').extract()).strip()
         posted = response.xpath('//p[@class="submitted"]/span[1]/text()').extract_first().strip()
         cp = response.xpath('//p[contains(@class, "dealer-name")]/strong/text()').extract_first()
-        desc = ''
+        desc = response.xpath('/html/body/aside/div[1]/main/section[2]/section/div[2]/div[2]/div[3]/p/text()').extract()
         ss = get_tld(response.url)
         year = ''
-        transmission = ''
+        transmission = response.xpath('/html/body/aside/div[1]/main/section[2]/section/div[2]/div[1]/div[4]/div/div[2]/span/text()').extract_first().strip()
+        engine_capacity = response.xpath('/html/body/aside/div[1]/main/section[2]/section/div[2]/div[1]/div[4]/div/div[4]/span/text()').extract_first()
         brand = ''
         model = ''
         tipe = ''
         ownership = ''
-        nego = ''
+        gps = ''
+        color = response.xpath('//*[@id="details"]/div[2]/div[1]/ul/li[1]/text()').extract_first().strip()
+        doors = response.xpath('//*[@id="details"]/div[2]/div[1]/ul/li[2]/text()').extract_first().strip()
+        airbags = response.xpath('//*[@id="Interior>"]/div[2]/div[1]/ul/li[1]/text()').extract_first().strip()
+        radio = response.xpath('//*[@id="Interior>"]/div[2]/div[2]/ul/li[1]/text()').extract_first().strip()
+        cd_player = response.xpath('//*[@id="Interior>"]/div[2]/div[2]/ul/li[2]/text()').extract_first().strip()
+        nego = response.xpath('/html/body/aside/div[1]/main/section[2]/section/div[2]/div[1]/div[2]/div[1]/p/text()').extract_first().strip()
         uploaded_by = ''
         phone = ''
         seen = ''
+
+        c.execute(
+            "insert into jualo_cars(url, title, city, province, description, price, contact_person, source_site, year, brand,  "
+            "model, type, ownership, engine_capacity, engine_type, transmission, doors, color, airbags, gps, radio, cd_player,  "
+            "posted, nego, uploaded_by, phone) "
+            "values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            (response.url, title, city, province, desc, price, cp, ss, year, brand, model, tipe, ownership,
+             engine_capacity, transmission, doors, color, airbags, gps, radio, cd_player, posted, nego,
+             uploaded_by, phone))
+        # self.db.commit()
 
         cars = {
             'url'           : response.url,
@@ -75,11 +94,18 @@ class Tes1(scrapy.Spider):
             'url'           : response.url,
             'source_site'   : ss,    
             'transmission'  : transmission,
+            'engine_capacity': engine_capacity,
             'brand'         : brand,
             'model'         : model,    
             'type'          : tipe,   
             'year'          : year,   
             'ownership'     : ownership,
+            'color'         : color,
+            'doors'         : doors,
+            'airbags'       : airbags,
+            'gps'           : gps,
+            'radio'         : radio,
+            'cd_player'     : cd_player,
             'nego'          : nego,    
             'uploaded_by'   : uploaded_by,   
             'phone'         : phone,   
